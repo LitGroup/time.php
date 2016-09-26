558475a81f536d8e4013c5529c1ae261e9843ecc
Roman Shamritskiy <roman@litgroup.ru>
2016-09-26 18:22:17 +0300

    Adds DateTIme interface.

---
+++ src/DateTime.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * This file is part of the "litgroup/datetime" package.
+ *
+ * (c) Roman Shamritskiy <roman@litgroup.ru>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+declare(strict_types = 1);
+
+namespace LitGroup\Time;
+
+interface DateTime
+{
+    public function getDate(): Date;
+
+    public function getTime(): Time;
+}
