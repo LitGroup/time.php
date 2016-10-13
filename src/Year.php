1979548e813f0e957eb040c047dc5515cbb0035c
Roman Shamritskiy <roman@litgroup.ru>
2016-10-13 16:56:47 +0300

    Refactoring.

--- src/Year.php
+++ src/Year.php
@@ -76,13 +76,6 @@ final class Year implements Equatable
 
     private function __construct(int $value)
     {
-        $this->setValue($value);
-    }
-
-    private function setValue(int $value)
-    {
-        assert($this->rawValue === null, 'Cannot be initialized twice.');
-
         if ($value < self::MIN_VALUE || $value > self::MAX_VALUE) {
             throw new DateTimeException(sprintf(
                 'Year cannot be less than %d or greater than %d, but %d given.',
